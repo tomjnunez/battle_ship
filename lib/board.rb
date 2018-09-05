cfd4103739e390ed7cd05ca53e63f545a637ff06
Tom Nunez <tomjnunez@gmail.com>
2018-09-05 16:01:04 -0600

    board and slot updated

--- lib/board.rb
+++ lib/board.rb
@@ -10,6 +10,35 @@ attr_reader :board
   end
 
   def board_display
+    puts "# 1 2 3 4"
     puts "A #{@board}\nB #{@board}\nC #{@board}\nD #{@board}".tr('"', '')
+  end
+
+  def vertical_board_display
+    {"1" => {"A" => @board[0][0], "B" => @board[1][0],
+             "C" => @board[2][0], "D" => @board[3][0]},
+
+     "2" => {"A" => @board[1][0], "B" => @board[1][1],
+             "C" => @board[2][1], "D" => @board[3][1]},
+
+     "3" => {"A" => @board[0][2], "B" => @board[1][2],
+             "C" => @board[2][2], "D" => @board[3][2]},
+
+     "4" => {"A" => @board[3][0], "B" => @board[1][3],
+             "C" => @board[2][3], "D" => @board[3][3]}}
+  end
+
+  def horizontal_board_display
+    {"A" => {"1" => @board[0][0], "2" => @board[0][1],
+             "3" => @board[0][2], "4" => @board[0][3]},
+
+     "B" => {"1" => @board[1][0], "2" => @board[1][1],
+             "3" => @board[1][2], "4" => @board[1][3]},
+
+     "C" => {"1" => @board[2][0], "2" => @board[2][1],
+             "3" => @board[2][2], "4" => @board[2][3]},
+
+     "D" => {"1" => @board[3][0], "2" => @board[3][1],
+             "3" => @board[3][2], "4" => @board[3][3]}}
   end
 end
--- test/board_test.rb
+++ test/board_test.rb
@@ -23,4 +23,39 @@ class BoardTest < Minitest::Test
 
     assert_equal 4, board.create_board
   end
+
+  def test_board_is_filled_with_slots
+    board = Board.new
+    board.create_board
+
+    assert_instance_of Slot, board.board[0][0]
+  end
+
+  def test_it_can_display_board_vertically
+    board = Board.new
+    board.create_board
+    a_1 = board.board[0][0]
+    b_2 = board.board[1][1]
+    c_3 = board.board[2][2]
+    d_4 = board.board[3][3]
+
+    assert_equal a_1, board.vertical_board_display["1"]["A"]
+    assert_equal b_2, board.vertical_board_display["2"]["B"]
+    assert_equal c_3, board.vertical_board_display["3"]["C"]
+    assert_equal d_4, board.vertical_board_display["4"]["D"]
+  end
+
+  def test_it_can_display_board_horizontally
+    board = Board.new
+    board.create_board
+    a_1 = board.board[0][0]
+    b_2 = board.board[1][1]
+    c_3 = board.board[2][2]
+    d_4 = board.board[3][3]
+
+    assert_equal a_1, board.horizontal_board_display["A"]["1"]
+    assert_equal b_2, board.horizontal_board_display["B"]["2"]
+    assert_equal c_3, board.horizontal_board_display["C"]["3"]
+    assert_equal d_4, board.horizontal_board_display["D"]["4"]
+  end
 end
